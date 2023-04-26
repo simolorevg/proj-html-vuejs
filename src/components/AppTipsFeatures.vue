0ae528fddac02c858858a6cf2f96409a76417c4e
simolorevg <simoneviaggi@libero.it>
2023-04-26 17:51:23 +0200

    continue working on the tips section

--- src/components/AppTipsFeatures.vue
+++ src/components/AppTipsFeatures.vue
@@ -6,7 +6,15 @@ export default {
 <template>
     <div class="my-tips-container d-flex">
         <div class="my-tips-text">
-            Lorem
+            <h2 class="my-tips-title">Get tips tricks on how to skyrocket your sales.</h2>
+            <p>Faff about only a quid blower I don't want no agro bleeding chimney pot burke tosser cras nice one boot
+                fanny!</p>
+            <div class="my-reporting-analysis">
+                <div class="my-reporting-image">
+                    <img src="" alt="">
+                </div>
+            </div>
+
         </div>
         <div class="my-tips-image">
             <img src="../assets/img/509.jpeg" alt="Team Tips">
@@ -19,15 +27,20 @@ export default {
 .my-tips-container {
     width: 80%;
     margin: 0 auto;
-    border: 1px solid red;
 
     .my-tips-text {
         width: 60%;
         border: 1px solid black;
+
+        .my-tips-title {
+            width: 70%;
+        }
     }
 
     .my-tips-image {
         flex-grow: 1;
+        border: 1px solid blue;
+        padding: 10px;
     }
 }
 </style>
