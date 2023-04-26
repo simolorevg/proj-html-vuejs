e049d1ff86802605eeecd127d5dfa372351f11d6
simolorevg <simoneviaggi@libero.it>
2023-04-26 12:29:49 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+
+export default {
+    components: {
+        AppHeader
+    }
+}
+</script>
+
+<template>
+    <AppHeader />
+</template>
+
+<style lang="scss" scoped>
+@use './style/general.scss' as*;
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+<template>
+    <h1>AppHeader</h1>
+</template>
+<style lang="scss" scoped>
+@use '../style/general.scss' as *;
+</style>
