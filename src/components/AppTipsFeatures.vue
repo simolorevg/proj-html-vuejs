ac5bb79aa129ff118c9b28a51407108552ac7554
simolorevg <simoneviaggi@libero.it>
2023-04-26 18:05:50 +0200

    tips main structure done

--- src/components/AppTipsFeatures.vue
+++ src/components/AppTipsFeatures.vue
@@ -9,12 +9,26 @@ export default {
             <h2 class="my-tips-title">Get tips tricks on how to skyrocket your sales.</h2>
             <p>Faff about only a quid blower I don't want no agro bleeding chimney pot burke tosser cras nice one boot
                 fanny!</p>
-            <div class="my-reporting-analysis">
+            <div class="my-reporting-analysis d-flex align-items-center">
                 <div class="my-reporting-image">
-                    <img src="" alt="">
+                    <img src="../assets/img/510.png" alt="Repo Image">
+                </div>
+                <div class="my-reporting-text">
+                    <h3 class="my-repo-title">Reporting Analysis</h3>
+                    <p>It is a long established fact that a reader will be distracted by the readble content of a page, when
+                        looking at its layout.</p>
                 </div>
             </div>
-
+            <div class="my-reporting-analysis d-flex align-items-center">
+                <div class="my-reporting-image">
+                    <img src="../assets/img/510.png" alt="Repo Image">
+                </div>
+                <div class="my-reporting-text">
+                    <h3>Reporting Analysis</h3>
+                    <p>It is a long established fact that a reader will be distracted by the readble content of a page, when
+                        looking at its layout.</p>
+                </div>
+            </div>
         </div>
         <div class="my-tips-image">
             <img src="../assets/img/509.jpeg" alt="Team Tips">
@@ -30,16 +44,35 @@ export default {
 
     .my-tips-text {
         width: 60%;
-        border: 1px solid black;
 
         .my-tips-title {
             width: 70%;
         }
     }
 
+    .my-reporting-analysis {
+        margin: 50px 0;
+
+        .my-repo-title {
+            color: blue;
+        }
+
+        .my-reporting-image {
+            width: 20%;
+
+            img {
+                width: 50px;
+                height: 50px;
+            }
+        }
+
+        .my-reporting-text {
+            flex-grow: 1;
+        }
+    }
+
     .my-tips-image {
         flex-grow: 1;
-        border: 1px solid blue;
         padding: 10px;
     }
 }
